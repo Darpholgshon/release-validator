e5413f32e0c9fcd298bd125f89fedc164f7af326
Darpholgshon <ralph.hodgson@gmail.com>
2014-09-23 11:25:15 +0100

    Simplest project ever

---
+++ src/main/java/org/hodgson/Application.java
@@ -0,0 +1,16 @@
+package org.hodgson;
+
+/**
+ * ****************************************************************************************
+ *
+ * @author <a href="ralph.hodgson@pressassociation.com">Ralph Hodgson</a>
+ * @since 23/09/2014 11:24
+ * <p/>
+ * ****************************************************************************************
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Helloworld");
+  }
+}
